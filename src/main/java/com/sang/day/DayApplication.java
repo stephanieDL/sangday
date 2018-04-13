7acd94a5fdd298cc5837de99117e7ceba16fe8c2
dankeiasahi <duanjiaxu@zillionfortune.com>
2018-04-13 10:18:12 +0800

    update nothing

---
+++ src/main/java/com/sang/day/DayApplication.java
@@ -0,0 +1,12 @@
+package com.sang.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sang/day/DayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sang.day;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
